24864e05fac35cd26615ee09dfc282ced8d126fa
necrolingus <leigh.williams88@gmail.com>
2024-10-29 08:38:43 +0200

    cacheadd all verbs

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,21 @@ import { generateJsonData } from '../controller/cacheJson.js'
 import express from 'express'
 const router = express.Router();
 
+//handler function cacheAdd on all the verbs
+function handleCacheAdd(req, res) {
+    const webhookEndpoint = req.params.webhookid;
+    const jsonData = generateJsonData(req);
+    const addOutcome = cacheAdd(webhookEndpoint, jsonData);
+
+    if (addOutcome === true) {
+        return res.status(200).send();
+    } else if (addOutcome === 0) {
+        return res.status(404).send("Add this cache key first before adding data to it");
+    } else {
+        return res.status(503).send();
+    }
+}
+
 //Create a unique key and set the cache. Respond with this key
 router.post("/create-endpoint", async function (req, res) {
     const { myUniqueKey } = req.body;
@@ -16,8 +31,8 @@ router.post("/create-endpoint", async function (req, res) {
     return res.status(200).send(`Your webhook endpoint ID: ${myUniqueKey}`)
 });
 
-//Delete a cache key
-router.delete("/:webhookid", async function (req, res) {
+//delete a cache key
+router.delete("/delete-endpoint/:webhookid", async function (req, res) {
     const webhookEndpoint = req.params.webhookid
     const delOutcome = cacheDelete(webhookEndpoint)
 
@@ -29,19 +44,10 @@ router.delete("/:webhookid", async function (req, res) {
 });
 
 //add data to cache
-router.post("/:webhookid", async function (req, res) {
-    const webhookEndpoint = req.params.webhookid
-    const jsonData = generateJsonData(req)
-    const addOutcome = cacheAdd(webhookEndpoint, jsonData)
-
-    if (addOutcome === true){
-        return res.status(200).send()
-    } else if (addOutcome === 0) {
-        return res.status(404).send("Add this cache key first before adding data to it")
-    } else {
-        return res.status(503).send()
-    }
-});
+router.post("/:webhookid", handleCacheAdd);
+router.put("/:webhookid", handleCacheAdd);
+router.patch("/:webhookid", handleCacheAdd);
+router.delete("/:webhookid", handleCacheAdd);
 
 //get all the data for a key
 router.get("/:webhookid", async function (req, res) {
