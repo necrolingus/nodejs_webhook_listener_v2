a642ce74e5ccbdd0786b6b3f9e4dd2ffdee126fa
necrolingus <leigh.williams88@gmail.com>
2024-11-04 14:14:18 +0200

    cache headers

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import { config } from "./controller/config.js";
 import express from 'express'
 import {router} from './routes/routes.js'
+import { headers } from './middleware/headers.js'
+
 const port = config.port
 
 const app = new express()
+app.disable('x-powered-by');
 app.use(express.json());
+app.use(headers);
 
 app.use('/webhooks', router)
 
